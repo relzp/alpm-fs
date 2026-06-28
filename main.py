eec09ddbe0f6a38bcdb9feedef30b6e22a9d10cd
relzp <161812588+relzp@users.noreply.github.com>
2026-06-28 20:14:46 +1000

    chore: improve comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument("root", help="folder to create hierarchy under")
     args = parser.parse_args()
 
+    # Get root dir from cli args, mkdir it and chdir to it
     root = args.root
     root_abs = os.path.abspath(root)
     try:
@@ -20,15 +21,13 @@ def main():
         if files:
             logging.error("Root directory already exists and is not empty, exiting")
             return
-
     logging.debug("creating hierarchy under: {}".format(root))
 
-    # Open local db
+    # Open local db and get list of locally-installed packages
     handle = Handle("/", "/var/lib/pacman")
     local_db = handle.get_localdb()
-
-    # Get installed pkgs in local db
     pkgs = local_db.pkgcache
+
     # Create hierarchy of symlinks for each package
     for pkg in pkgs:
         name = pkg.name
@@ -36,14 +35,17 @@ def main():
         os.mkdir(name)
         os.chdir(name)
 
+        # Filter files to only entries not a prefix of the next file
+        # First perform a lexicographic sort
         files = sorted(pkg.files)
-        # Filter file list to only entries not a prefix of another entry
         files_no_prefixes = []
+        # For each path, only include if not a prefix of the next path
         for idx, (path, size, mode) in enumerate(files):
             if idx + 1 < len(files):
                 next_path = files[idx + 1][0]
                 if next_path.startswith(path):
                     continue
+            # Remove trailing slash from dirs to avoid issues in next section
             path = path.removesuffix("/")
             files_no_prefixes.append(path)
 
@@ -56,10 +58,12 @@ def main():
                 logging.debug("making dir {}".format(path_dir))
                 os.makedirs(path_dir, exist_ok=True)
 
+            # Construct path under root dir, then symlink to actual file
             actual_file_path = os.path.join("/", path)
             logging.debug("making symlink {} -> {}".format(path, actual_file_path))
             os.symlink(actual_file_path, path)
 
+        # Return to root dir
         os.chdir("..")
 
 if __name__ == "__main__":
